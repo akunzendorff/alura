aad415bffe364e45955142bc1408ac0ebcba94d6
Ana Flávia V. Kunzendorff <142523426+akunzendorff@users.noreply.github.com>
2025-02-27 00:19:26 -0300

    .

--- python/orientação-a-objetos/oo-sabor-express/modelos/avaliacao.py
+++ python/orientação-a-objetos/oo-sabor-express/modelos/avaliacao.py
@@ -5,11 +5,4 @@ class Avaliacao:
         self.nota = nota
 
 
-    @property #torna capaz ler as informações
-    def media_avaliacao(self):
-        if not self._avaliacao:
-            return 0
-        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
-        quantidade_de_notas = len(self._avaliacao)
-        media = round(soma_das_notas / quantidade_de_notas, 1)
-        return media
+    
